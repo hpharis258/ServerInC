de605659237cce5e7e113dc4c4b3e60e9e84494d
Haroldas Varanauskas <hpharis258@gmail.com>
2026-02-18 20:24:46 +0000

    LRU Cache

--- server.c
+++ server.c
@@ -10,8 +10,15 @@
 // Goal -> To Learn about how a web server works under the hood, don't use AI slop generator.
 // Base knowledge on https://github.com/bloominstituteoftechnology/C-Web-Server
 
-// Started on 17/06/2024 -> Read https://github.com/bloominstituteoftechnology/C-Web-Server/blob/master/guides/net.md
-//
+// 1. Started on 17/06/2024 -> Read https://github.com/bloominstituteoftechnology/C-Web-Server/blob/master/guides/net.md
+// 2. Started on 18/06/2024 -> Already knew what a doubly linked list is, but read about it anyway https://github.com/bloominstituteoftechnology/C-Web-Server/blob/master/guides/dllist.md
+// Wtf is an LRU cache? -> https://github.com/bloominstituteoftechnology/C-Web-Server/blob/master/guides/lrucache.md
+// In the context of a webserver, when a file is requested by a client, the following steps occur:
+// Check if the file is in the cache.
+// If it is, serve it. We're done.
+// If it's not in the cache, load it from disk.
+// Save it in the cache.
+// Serve it.
 
 int main()
 {
